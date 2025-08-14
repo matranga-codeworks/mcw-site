689ae2bab2dd319473df9dde0b19c6cfa49222b7
Frank Matranga <thefrankmatranga@gmail.com>
2025-08-14 07:50:36 -0400

    Add SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://matrangacodeworks.com",
+
   prefetch: {
-    prefetchAll: true
+    prefetchAll: true,
   },
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [sitemap()],
 });
